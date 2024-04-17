f8950183e36042b72910d6e1c5a6bc276cff2487
unknown <dnthoang1295@gmail.com>
2024-04-17 21:22:11 +0700

    test add 2

--- src/HomePage.js
+++ src/HomePage.js
@@ -4,7 +4,7 @@ function HomePage(props) {
     return (
         <div>
             <p>code cua abcdefgh</p>
-            <p>test code</p>
+            <p>test codeaasdasd s123</p>
             <p>COde cua toi moi code123123123123</p>
             <h1>This is home page</h1>
         </div>
