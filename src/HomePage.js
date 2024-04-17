48f8be3b931551edc6a441e230a20effb2e61731
XuyenLe <letuanxuyen92@gmail.com>
2024-04-17 21:12:14 +0700

    edit homepage

--- src/HomePage.js
+++ src/HomePage.js
@@ -3,6 +3,7 @@ import React from 'react';
 function HomePage(props) {
     return (
         <div>
+            <p>COde cua toi moi code</p>
             <h1>This is home page</h1>
         </div>
     );
