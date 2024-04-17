e413ea49f7c34e4e8c813db5e89f06f49e2508d5
unknown <congkhoitran01@gmail.com>
2024-04-17 21:31:27 +0700

    khoi

--- src/HomePage.js
+++ src/HomePage.js
@@ -6,6 +6,7 @@ function HomePage(props) {
             <p>code cua abcdefgh</p>
             <p>COde cua toi moi code</p>
             <h1>This is home page</h1>
+            <h2>khoi</h2>
             <h2>Hello thay</h2>
         </div>
     );
