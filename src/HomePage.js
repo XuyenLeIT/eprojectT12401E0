1bb574818dbe0a37bc1509df3cef54707dca5407
unknown <dnthoang1295@gmail.com>
2024-04-17 21:30:02 +0700

    test 2

---
+++ src/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function HomePage(props) {
+    return (
+        <div>
+            <p>code cua abcdefgh</p>
+            <p>COde cua toi moi code</p>
+            <h1>This is home page</h1>
+            <h1>test 2</h1>
+        </div>
+    );
+}
+
+export default HomePage;
